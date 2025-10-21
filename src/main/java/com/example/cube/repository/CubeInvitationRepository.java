4fdd9308867a83c5e5b5960f54d9a536757f73b2
Robel Endashaw <rendasha@umd.edu>
2025-10-21 16:09:46 -0400

    doesnt matter

--- src/main/java/com/example/cube/repository/CubeInvitationRepository.java
+++ src/main/java/com/example/cube/repository/CubeInvitationRepository.java
@@ -3,11 +3,19 @@ package com.example.cube.repository;
 import com.example.cube.model.CubeInvitation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CubeInvitationRepository extends JpaRepository<CubeInvitation, UUID> {
 
     boolean existsByEmailAndCubeIdAndStatus(String email, UUID cubeId, String status);
+    boolean existsByEmailAndCubeIdAndStatusId(String email, UUID cubeId, Integer statusId);
+    Optional<CubeInvitation> findByInviteToken(String inviteToken);
+    List<CubeInvitation> findByCubeIdAndStatus(UUID cubeId, String status);
+    List<CubeInvitation> findByInviteeIdAndStatus(UUID inviteeId, String status);
+    List<CubeInvitation> findByEmailAndStatus(String email, String status);
 
 }
