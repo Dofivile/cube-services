641625a9ad936b586c91f678995433cb42f54b6d
Robel <robelsmac@Robels-MacBook-Air.local>
2025-11-11 20:49:04 -0500

    testtin

--- src/main/java/com/example/cube/service/impl/StripeConnectServiceImpl.java
+++ src/main/java/com/example/cube/service/impl/StripeConnectServiceImpl.java
@@ -137,7 +137,7 @@ public class StripeConnectServiceImpl implements StripeConnectService {
             AccountLinkCreateParams params = AccountLinkCreateParams.builder()
                     .setAccount(accountId)
                     .setRefreshUrl("https://cube-service-prod-788967711773.us-central1.run.app/api/stripe/onboarding/refresh")
-                    .setReturnUrl("ttps://cube-service-prod-788967711773.us-central1.run.app/api/stripe/onboarding/complete")
+                    .setReturnUrl("https://cube-service-prod-788967711773.us-central1.run.app/api/stripe/onboarding/complete")
                     .setType(AccountLinkCreateParams.Type.ACCOUNT_ONBOARDING)
                     .setCollect(AccountLinkCreateParams.Collect.EVENTUALLY_DUE)
                     .build();
